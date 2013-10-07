8a8f00179b241e58fe2d1967874d666cc8bbe3e9
Romain Sertelon <romain@sertelon.fr>
2013-10-07 14:46:23 +0200

    Adds a TryWrapper
    Allows one to wrap the execution of a function in a try catch to get a Try<T>

--- src/main/java/fr/bluepyth/java/funchelper/trylike/FTry.java
+++ src/main/java/fr/bluepyth/java/funchelper/trylike/FTry.java
@@ -1,7 +1,21 @@
 package fr.bluepyth.java.funchelper.trylike;
 
+import static fr.bluepyth.java.funchelper.trylike.Try.failure;
+import static fr.bluepyth.java.funchelper.trylike.Try.success;
 import fr.bluepyth.java.funchelper.function.F1;
 
 public interface FTry<I, O> extends F1<I, Try<O>> {
-
+	
+	public static abstract class TryWrapper<U,V> implements FTry<U, V> {
+		@Override
+		public Try<V> apply(U input) {
+			try {
+				return success(wrap(input));
+			} catch (Exception e) {
+				return failure(e);
+			}
+		}
+		
+		public abstract V wrap(U input) throws Exception;
+	}
 }
