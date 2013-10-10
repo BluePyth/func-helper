fdd0f8d2f408166c411a69a7b43e92ccc488d73c
Romain Sertelon <romain@sertelon.fr>
2013-10-10 16:56:10 +0200

    Renames nothing() method to avoid name collision

--- src/main/java/fr/bluepyth/java/funchelper/trylike/FTry.java
+++ src/main/java/fr/bluepyth/java/funchelper/trylike/FTry.java
@@ -10,7 +10,7 @@ public interface FTry<I, O> extends F1<I, Try<O>> {
 	
 	public static class Predef {
 	
-		public static <A> FTry<A, Nothing> nothing() {
+		public static <A> FTry<A, Nothing> nothing1() {
 			return new FTry<A, Nothing>() {
 				public Try<Nothing> apply(A input) {
 					return emptySuccess();
