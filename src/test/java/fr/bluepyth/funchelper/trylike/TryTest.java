1fd765a51d3cfbc6c37ce5f1925076a5b6021f42
Romain Sertelon <romain@sertelon.fr>
2013-10-14 15:48:38 +0200

    Imports cleaning

--- src/test/java/fr/bluepyth/funchelper/trylike/TryTest.java
+++ src/test/java/fr/bluepyth/funchelper/trylike/TryTest.java
@@ -2,8 +2,8 @@ package fr.bluepyth.funchelper.trylike;
 
 import static fr.bluepyth.funchelper.immutable.IList.list;
 import static fr.bluepyth.funchelper.option.Opt.toOpt;
-import static fr.bluepyth.funchelper.trylike.Try.toTry;
 import static fr.bluepyth.funchelper.trylike.Try.success;
+import static fr.bluepyth.funchelper.trylike.Try.toTry;
 import static fr.bluepyth.funchelper.trylike.Try.trySeq;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -14,7 +14,6 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 
 import fr.bluepyth.funchelper.immutable.IList;
 import fr.bluepyth.funchelper.option.Opt;
-import fr.bluepyth.funchelper.trylike.Try;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class TryTest {
