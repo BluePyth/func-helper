55acd77725d925713d022af728336334ea5943df
Romain Sertelon <romain@sertelon.fr>
2013-11-05 08:37:46 +0100

    Adds then for F0

--- src/main/java/fr/bluepyth/funchelper/function/F0.java
+++ src/main/java/fr/bluepyth/funchelper/function/F0.java
@@ -1,5 +1,15 @@
 package fr.bluepyth.funchelper.function;
 
-public interface F0<O> {
-	O apply();
+public abstract class F0<O> {
+	public abstract O apply();
+	
+	public <A> F0<A> then(final F1<O,A> f) {
+		return new F0<A>() {
+			@Override
+			public A apply() {
+				return f.apply(F0.this.apply());
+			}
+		};
+		
+	}
 }
