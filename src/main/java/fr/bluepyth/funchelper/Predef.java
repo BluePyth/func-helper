060e72a0ac5a02a00de860bc66b312dc8573500a
Romain Sertelon <romain@sertelon.fr>
2013-11-15 11:24:18 +0100

    Adds another predefined function

--- src/main/java/fr/bluepyth/funchelper/Predef.java
+++ src/main/java/fr/bluepyth/funchelper/Predef.java
@@ -39,6 +39,12 @@ public final class Predef {
 		}
 	};
 
+	public static final F1<String, Boolean> isStrNotEmpty = new F1<String, Boolean>() {
+		public Boolean apply(String input) {
+			return !input.isEmpty();
+		}
+	};
+
 	public static final F1<Object, String> objToString = new F1<Object, String>() {
 		public String apply(Object input) {
 			return input.toString();
