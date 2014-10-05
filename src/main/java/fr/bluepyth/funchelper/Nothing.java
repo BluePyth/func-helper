ccf2710f8c334143aa7c04b57be5a12f4a9c792c
Romain Sertelon <romain@sertelon.fr>
2014-10-05 22:53:01 +0200

    Forgot to remove files

--- src/main/java/fr/bluepyth/funchelper/Nothing.java
+++
@@ -1,7 +0,0 @@
-package fr.bluepyth.funchelper;
-
-public class Nothing {
-	public static final Nothing nothing = new Nothing();
-	
-	private Nothing() {}
-}
--- src/main/java/fr/bluepyth/funchelper/function/F1.java
+++
@@ -1,9 +0,0 @@
-package fr.bluepyth.funchelper.function;
-
-public interface F1<I, O> {
-	O apply(I input);
-	
-	default public <A> F1<I, A> then(final F1<O, A> next) {
-		return input -> next.apply(F1.this.apply(input));
-	}
-}
